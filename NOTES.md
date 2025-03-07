229fb3c056cf0e5262e228319a1b0d57c6661431
Nathaniel Jackson <njackson@student.42adel.org.au>
2025-03-07 14:24:16 +1030

    Wordpress setup

--- NOTES.md
+++ NOTES.md
@@ -1,6 +1,15 @@
 
 
-docker commands
+# Docker Commands
 - `docker container ls` list active containers
 - `docker volume prune` remove unused docker volumes
 - `docker exec -it <container-name> bash` for interactive shell
+
+
+# to do
+- change nginx to use docker secrets to get the intra id
+- mariadb
+
+optional
+- remove the security risk from first access of the webpage
+- make the wordpress site cool and epic 😎
---
+++ srcs/requirements/wordpress/scripts/init-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -e /var/www/html/wp-config.php ]; then
+
+    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+    DB_NAME=$(cat /run/secrets/DB_NAME)
+    DB_USER=$(cat /run/secrets/DB_USER)
+    DB_PASSWORD=$(cat /run/secrets/DB_PASSWORD)
+    DB_HOST=inception-mariadb
+
+    sed -i "s/database_name_here/${DB_NAME}/" /var/www/html/wp-config.php
+    sed -i "s/username_here/${DB_USER}/" /var/www/html/wp-config.php
+    sed -i "s/password_here/${DB_PASSWORD}/" /var/www/html/wp-config.php
+    sed -i "s/localhost/${DB_HOST}/" /var/www/html/wp-config.php
+
+fi
+
+php-fpm82 -F
