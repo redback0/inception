f212ec6564189754980d928ecd5e57c984947a38
Nathaniel Jackson <njackson@student.42adel.org.au>
2025-03-07 15:27:48 +1030

    moved example envs

--- srcs/requirements/nginx/scripts/init-nginx.sh
+++ srcs/requirements/nginx/scripts/init-nginx.sh
@@ -2,13 +2,15 @@
 
 if [ ! -f /etc/ssl/certs/inception_certificate.csr ]; then
 
+    echo "--FIRST RUN--"
+
     #INTRA_ID=$(cat /run/secrets/INTRA_ID)
     set -a && source /run/secrets/.env && set +a
 
     openssl req -newkey rsa:2048 -noenc\
         -keyout /etc/ssl/inception_private_key.key -x509 -days 365\
         -out /etc/ssl/certs/inception_certificate.csr\
-        -subj '/CN='${INTRA_ID}'.42.fr'
+        -subj '/CN='${INTRA_ID}'.42.fr' > /dev/null
 
     sed -i "s/INTRA_ID/${INTRA_ID}/g" /etc/nginx/http.d/nginx.conf
 
