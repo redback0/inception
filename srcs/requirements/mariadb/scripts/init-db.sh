426938c8b851af8f686d0077015a226a1fc9f073
Nathaniel Jackson <njackson@student.42adel.org.au>
2025-03-12 18:48:03 +1030

    Attempt to initialize database

---
+++ srcs/requirements/mariadb/scripts/init-db.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+FIRST_RUN_FILE=/etc/first_run_file
+
+if [ ! -f $FIRST_RUN_FILE ]; then
+    touch $FIRST_RUN_FILE
+
+    set -a && source /run/secrets/wordpress.env \
+        && source /run/secrets/database.env && set +a
+
+    WP_HOST=inception-wordpress.srcs_wordpress-network
+
+    sed -i "s/DB_NAME/$DB_NAME/g" /run/scripts/init-db.sql
+    sed -i "s/DB_USER/$DB_USER/g" /run/scripts/init-db.sql
+    sed -i "s/DB_PASSWORD/$DB_PASSWORD/g" /run/scripts/init-db.sql
+    sed -i "s/WP_HOST/$WP_HOST/g" /run/scripts/init-db.sql
+
+    mariadb < /run/scripts/init-db.sql
+fi
+
+mariadbd --datadir=/var/mariadb/data --user=mysql
